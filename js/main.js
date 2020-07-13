ea1fa5c8cb9608f256b500976a33905963d2040b
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-13 09:04:45 +0200

    Refactor main js and solve issues

--- js/main.js
+++ js/main.js
@@ -1,31 +1,49 @@
 'use strict';
 
-const number = document.querySelector('.js-number-introduce');
-const button = document.querySelector('.js-submit-button');
-const clue = document.querySelector('.js-clue');
-let attemps = document.querySelector('.js-attepms');
-let randomNumber = getRandomNumber(100);
+// random number
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-console.log(randomNumber);
 
-function compareNumber(ev) {
+const randomNumber = getRandomNumber(100);
+
+console.log('Mi número aleatorio es:', randomNumber);
+
+// handle event
+
+const submitBtn = document.querySelector('.js-submit');
+const number = document.querySelector('.js-number');
+const clue = document.querySelector('.js-clue');
+const counter = document.querySelector('.js-counter');
+
+function handleSubmitBtn(ev) {
   ev.preventDefault();
-  let numberValue = number.value;
-  console.log(numberValue);
-  attemps++;
+  updateClue();
+  updateCounter();
+}
 
-  if (numberValue > randomNumber && numberValue < 100) {
-    clue.innerHTML = 'Demasiado alto';
-  } else if (numberValue < randomNumber) {
-    clue.innerHTML = 'Demasiado bajo';
-  } else if (numberValue === randomNumber) {
-    clue.innerHTML = '¡¡¡Has ganado, Campeona!!!';
+function updateClue() {
+  let numberValue = number.value;
+  numberValue = parseInt(numberValue);
+  if (numberValue >= 1 && numberValue <= 100) {
+    if (numberValue < randomNumber) {
+      clue.innerHTML = 'Demasiado bajo.';
+    } else if (numberValue > randomNumber) {
+      clue.innerHTML = 'Demasiado alto.';
+    } else if (numberValue === randomNumber) {
+      clue.innerHTML = 'Has ganado campeona!!!';
+    }
   } else {
     clue.innerHTML = 'El número debe estar entre 1 y 100';
   }
 }
 
-button.addEventListener('click', compareNumber);
+let counterAttempts = 0;
+
+function updateCounter() {
+  counterAttempts++;
+  counter.innerHTML = counterAttempts;
+}
+
+submitBtn.addEventListener('click', handleSubmitBtn);
